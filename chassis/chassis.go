4fa254f1af3e27d4c3add5cdd4817539abf59af7
Anna <me@annahaig.com>
2022-02-07 22:32:20 +0000

    open trace and chassis serve

--- chassis/chassis.go
+++ chassis/chassis.go
@@ -1,8 +1,22 @@
 package chassis
 
+import (
+	"context"
+	"fmt"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 type EventSourceChassis struct {
-	Requests *Requests
-	Events   *Events
+	Requests      *Requests
+	Events        *Events
+	traceProvider *sdktrace.TracerProvider
 }
 
 func NewEventSourceChassis() *EventSourceChassis {
@@ -14,4 +28,35 @@ func NewEventSourceChassis() *EventSourceChassis {
 			NewEventManager(nil),
 		},
 	}
+}
+
+func (c *EventSourceChassis) ConfigureOpenTelemetry(serviceName string, exp sdktrace.SpanExporter, attrs ...attribute.KeyValue) {
+	attrs = append(attrs, semconv.ServiceNameKey.String(serviceName))
+	currentResource := resource.NewWithAttributes(
+		semconv.SchemaURL,
+		attrs...,
+	)
+	c.traceProvider = sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exp),
+		sdktrace.WithResource(currentResource))
+
+	otel.SetTracerProvider(c.traceProvider)
+
+	c.traceProvider.Tracer(serviceName)
+}
+
+func (c *EventSourceChassis) ReadyAndServe(ctx context.Context) <-chan bool {
+	done := make(chan bool, 1)
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		defer c.traceProvider.Shutdown(ctx)
+
+		sig := <-sigs
+		fmt.Println("Shutting Down: " + sig.String())
+		done <- true
+	}()
+
+	return done
 }
--- chassis/go.mod
+++ chassis/go.mod
@@ -3,14 +3,17 @@ module github.com/teken/GoMicroService/chassis
 go 1.18
 
 require (
-	github.com/reiver/go-pathmatch v2.2.0+incompatible
 	github.com/stretchr/testify v1.7.0
+	go.opentelemetry.io/otel/sdk v1.3.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
-	github.com/fatih/structs v1.1.0 // indirect
+	github.com/go-logr/logr v1.2.1 // indirect
+	github.com/go-logr/stdr v1.2.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.1.0 // indirect
+	go.opentelemetry.io/otel v1.3.0 // indirect
+	go.opentelemetry.io/otel/trace v1.3.0 // indirect
+	golang.org/x/sys v0.0.0-20210423185535-09eb48e85fd7 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
